67119d8cc4994852ae961851c8c1891e6daac5c2
brutus <iceforge@protonmail.com>
2019-07-25 16:33:13 -0400

    ?

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
+
+// LIBS AND FRAMEWORKS
 const ex = require('express');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const sequelize = require('./util/db');
-
 let bodyParser = require('body-parser');
-// let dirname = path.dirname(process.mainModule.filename); 
-
 const app = ex();
 
+// MODELS
+const Product = require('./models/shop');
+const User = require('./models/user');
+const CartItem = require('./models/cart-item');
+const Cart = require('./models/cart');
+
+
+// ROUTES
 const shopRoutes = require('./routes/shop');
 const adminRoutes = require('./routes/admin');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
+// MIDDLEWARE ENGINES
 app.use(expressLayouts);
 app.set('view engine','ejs');
 app.set('views','views');
-// console.log(__dirname);
 
+// PATH
 app.use('/public',ex.static(path.join(__dirname, 'public')));
 app.use('/script',ex.static(path.join(__dirname,'node_modules')));
 app.use('/media',ex.static(path.join(__dirname,'media')));
 
 
 
-app.use('/admin',adminRoutes);
-app.use(shopRoutes);
+
+
+// MYSQL 
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});
 
 sequelize
+// .sync({force:true})
 .sync()
 .then((result) => {
     // console.log(result);
@@ -34,4 +52,6 @@ sequelize
 .catch(err => {
     // console.log(err);
 })
+
+//LISTEN
 app.listen(3000);
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/db');
+
+const User = sequelize.define('user',{
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true
+    },
+    name:Sequelize.STRING,
+    lname:Sequelize.STRING,
+    pass:Sequelize.STRING,
+    email:Sequelize.STRING
+})
+
+module.exports = User;
